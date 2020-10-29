7a61483168aa1861816ac81ebc50ef43d0025b89
merel mourik <merelmourik@student.42.fr>
2020-10-29 15:03:44 +0100

    nonchelante rund van mysql, php en wordpress werkt

--- setup.sh
+++ setup.sh
@@ -1,15 +1,13 @@
 # deleting the current minikube, if it exists
 minikube delete
 
-# starting minikube's original driver, which is virutalbox
+# starting minikube's original driver, which is virutalbox and
 minikube start --driver=virtualbox \
                 --cpus=2 --memory=2048 --disk-size=10g \
                 --addons metallb \
                 --addons dashboard
 
 # enabling MetalLB and kubernetes dashboard
-# minikube addons enable metallb
-# minikube addons enable dashboard
 
 # preparing MetalLB
 kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
@@ -18,23 +16,25 @@ kubectl apply -f srcs/metallb/config.yaml
 # connecting to the docker environment that is running in the cluster
 eval $(minikube docker-env)
 
-# docker build -t mynginx ./srcs/nginx
-# kubectl apply -f ./srcs/nginx/nginx.yaml
+# building my mymysql image and as
+docker build -t mymysql ./srcs/mysql
+# adding the mysql.yaml file to deploy mysql container
+kubectl apply -f ./srcs/mysql/mysql.yaml
+
 # building my phpmyadmin image
-# docker build -t myphpmyadmin ./srcs/phpmyadmin
+docker build -t myphpmyadmin ./srcs/phpmyadmin
 
 # adding the phpmyadmin.yaml file to deploy the myphpmyadmin container
-# kubectl apply -f ./srcs/phpmyadmin/phpmyadmin.yaml
+kubectl apply -f ./srcs/phpmyadmin/phpmyadmin.yaml
 
 # building my wordpress image
-# docker build -t mywordpress ./srcs/wordpress
+docker build -t mywordpress ./srcs/wordpress
 
 # adding the worpress.yaml file to deploy mywordpress container
-# kubectl apply -f ./srcs/wordpress/wordpress.yaml
+kubectl apply -f ./srcs/wordpress/wordpress.yaml
 
-# adding the mysql.yaml file to deploy mysql container
-docker build -t mymysql ./srcs/mysql_test
-kubectl apply -f ./srcs/mysql_test/mysql.yaml
+# docker build -t mynginx ./srcs/nginx
+# kubectl apply -f ./srcs/nginx/nginx.yaml
 
 # adding the phpmyadmin.yaml file to deploy phpmyadmin container
 
