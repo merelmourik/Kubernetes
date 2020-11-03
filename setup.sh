ff337f1d60975cabe416ee0de0ee07a3da40d990
merel mourik <merelmourik@student.42.fr>
2020-11-03 22:09:59 +0100

    nginx redirections work, choose which wordpress you want to use

--- setup.sh
+++ setup.sh
@@ -2,35 +2,39 @@
 minikube delete
 
 # starting minikube's original driver, which is virutalbox and
-minikube start --driver=virtualbox \
+# enabling MetalLB and kubernetes dashboard
+minikube start 	--driver=virtualbox \
                 --cpus=2 --memory=2048 --disk-size=10g \
                 --addons metallb \
                 --addons dashboard
 
-# enabling MetalLB and kubernetes dashboard
-
 # preparing MetalLB
 kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
 kubectl apply -f srcs/metallb/config.yaml
+# waar is dit voor?
 
 # connecting to the docker environment that is running in the cluster
 eval $(minikube docker-env)
 
 # creating my service account
-kubectl create serviceaccount mmourik			# waar is dit voor nodig?
+kubectl create serviceaccount mmourik			# waar is dit voor nodig? mysql?
 kubectl apply -f srcs/service_account.yaml
 
 # building my nginx image and deploying the container
-# docker build -t mynginx ./srcs/nginx
-# kubectl apply -f ./srcs/nginx/nginx.yaml
+docker build -t nginx ./srcs/nginx
+kubectl apply -f ./srcs/nginx/nginx.yaml
 
-# # building my mysql image and deploying the container
+# building my mysql image and deploying the container
 docker build -t mysql ./srcs/mysql
 kubectl apply -f ./srcs/mysql/mysql.yaml
 
 # # building my wordpress image and deploying the container
-docker build -t wordpress ./srcs/wordpress_mark
-kubectl apply -f ./srcs/wordpress_mark/wordpress.yaml
+docker build -t wordpress ./srcs/wordpress_3
+kubectl apply -f ./srcs/wordpress_3/wordpress.yaml
+
+# building my ftps image and deploying the container
+docker build -t ftps ./srcs/ftps
+kubectl apply -f ./srcs/ftps/ftps.yaml
 
 # building my phpmyadmin image and deploying the container
 # docker build -t phpmyadmin ./srcs/phpmyadmin
