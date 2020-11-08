b3794f8d17ed2e30a422fcae8a9b4996a04c425f
Merel Mourik <mmourik@f0r5s3.codam.nl>
2020-11-08 14:19:14 +0100

    nginx->php works with php from rpt

--- setup.sh
+++ setup.sh
@@ -11,34 +11,35 @@ minikube start 	--driver=virtualbox \
 # preparing MetalLB
 kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
 kubectl apply -f srcs/metallb/config.yaml
-# waar is dit voor?
 
 # connecting to the docker environment that is running in the cluster
 eval $(minikube docker-env)
 
 # creating my service account
-kubectl create serviceaccount mmourik			# waar is dit voor nodig? mysql?
+kubectl create serviceaccount mmourik	
 kubectl apply -f srcs/service_account.yaml
 
 # building my nginx image and deploying the container
 docker build -t nginx ./srcs/nginx
 kubectl apply -f ./srcs/nginx/nginx.yaml
 
+# building my phpmyadmin image and deploying the container
+docker build -t phpmyadmin ./srcs/phpmyadmin_r
+kubectl apply -f ./srcs/phpmyadmin_r/phpmyadmin.yaml
+
+
 # # building my ftps image and deploying the container
 # docker build -t ftps ./srcs/ftps
 # kubectl apply -f ./srcs/ftps/ftps.yaml
 
-# building my mysql image and deploying the container
-docker build -t mysql ./srcs/mysql
-kubectl apply -f ./srcs/mysql/mysql.yaml
+# # building my mysql image and deploying the container
+# docker build -t mysql ./srcs/mysql
+# kubectl apply -f ./srcs/mysql/mysql.yaml
 
 # # building my wordpress image and deploying the container
 docker build -t wordpress ./srcs/wordpress
 kubectl apply -f ./srcs/wordpress/wordpress.yaml
 
-# building my phpmyadmin image and deploying the container
-docker build -t phpmyadmin ./srcs/phpmyadmin
-kubectl apply -f ./srcs/phpmyadmin/phpmyadmin.yaml
 
 # # building my influxdb image and deploying the container
 # docker build -t influxdb ./srcs/influxdb
