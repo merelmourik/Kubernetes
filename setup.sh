fc4f0e383e7bdf4c7a2b42037c39660c5b683d30
merel mourik <merelmourik@student.42.fr>
2020-11-09 16:40:59 +0100

    some more cleaning, nginx fails sometimes (since last commit at school) and telegraf is also not too stable

--- setup.sh
+++ setup.sh
@@ -1,8 +1,7 @@
 # deleting the current minikube, if it exists
 minikube delete
 
-# starting minikube's original driver, which is virutalbox and
-# enabling MetalLB and kubernetes dashboard
+# starting minikube's original driver, which is virutalbox and enabling MetalLB and kubernetes' dashboard
 minikube start 	--driver=virtualbox \
                 --cpus=2 --memory=2048 --disk-size=10g \
                 --addons metallb \
@@ -27,26 +26,26 @@ kubectl apply -f ./srcs/nginx/nginx.yaml
 docker build -t phpmyadmin ./srcs/phpmyadmin
 kubectl apply -f ./srcs/phpmyadmin/phpmyadmin.yaml
 
-# # building my ftps image and deploying the container
-# docker build -t ftps ./srcs/ftps
-# kubectl apply -f ./srcs/ftps/ftps.yaml
+# building my ftps image and deploying the container
+docker build -t ftps ./srcs/ftps
+kubectl apply -f ./srcs/ftps/ftps.yaml
 
 # building my mysql image and deploying the container
 docker build -t mysql ./srcs/mysql
 kubectl apply -f ./srcs/mysql/mysql.yaml
 
-# # building my wordpress image and deploying the container
+# building my wordpress image and deploying the container
 docker build -t wordpress ./srcs/wordpress
 kubectl apply -f ./srcs/wordpress/wordpress.yaml
 
-# # building my influxdb image and deploying the container
-# docker build -t influxdb ./srcs/influxdb
-# kubectl apply -f ./srcs/influxdb/influxdb.yaml
+# building my influxdb image and deploying the container
+docker build -t influxdb ./srcs/influxdb
+kubectl apply -f ./srcs/influxdb/influxdb.yaml
 
-# # building my telegraf image and deploying the container
-# docker build -t telegraf ./srcs/telegraf
-# kubectl apply -f ./srcs/telegraf/telegraf.yaml
+# building my telegraf image and deploying the container
+docker build -t telegraf ./srcs/telegraf
+kubectl apply -f ./srcs/telegraf/telegraf.yaml
 
-# # building my grafana image and deploying the container
-# docker build -t grafana ./srcs/grafana
-# kubectl apply -f ./srcs/grafana/grafana.yaml
+# building my grafana image and deploying the container
+docker build -t grafana ./srcs/grafana
+kubectl apply -f ./srcs/grafana/grafana.yaml
